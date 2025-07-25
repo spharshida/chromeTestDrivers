98e0bdd63aa37dc89c009f512e18f698256bbe36
S P Harshida <108288680+spharshida@users.noreply.github.com>
2025-07-25 10:53:48 +0530

    Update caseStudyNavigation.java

--- src/com/seleniumBrowser/caseStudyNavigation.java
+++ src/com/seleniumBrowser/caseStudyNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class caseStudyNavigation {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args){
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
